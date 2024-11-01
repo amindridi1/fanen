62baf8e1d09b8deacd1c4b90a179a5cffc314d4d
amindridi1 <125443150+amindridi1@users.noreply.github.com>
2024-11-01 12:55:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,26 +2,17 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins='*', ping_timeout=5, ping_interval=25)
+app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('connect')
-def handle_connect():
-    print('Client connected')
-
-@socketio.on('disconnect')
-def handle_disconnect():
-    print('Client disconnected')
-
 @socketio.on('message')
 def handle_message(msg):
-    """Broadcasts received messages to all connected clients."""
-    print(f"Message: {msg}")
+    print('Message: ' + msg)
     send(msg, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app)
