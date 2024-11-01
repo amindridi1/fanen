246f75743216dcc0d89b11df71637a86dc98b61f
amindridi1 <125443150+amindridi1@users.noreply.github.com>
2024-11-01 14:44:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,44 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, request, session
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "mysecret"
 socketio = SocketIO(app)
 
-@app.route('/')
+users = []
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    if "username" in session:
+        return render_template("chatroom.html", username=session["username"])
+    else:
+        return render_template("index.html")
 
-@socketio.on('message')
-def handle_message(msg):
-    print('Received message: ' + msg)
-    socketio.send(msg)  # Echo the message back to clients
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    session["username"] = username
+    users.append(username)
+    return render_template("chatroom.html", username=username)
 
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    users.remove(session["username"])
+    return render_template("index.html")
+
+@socketio.on("send_message")
+def handle_send_message(data):
+    emit("receive_message", {"username": session["username"], "message": data["message"]}, broadcast=True)
+
+@socketio.on("connect")
+def handle_connect():
+    emit("user_joined", {"username": session["username"]}, broadcast=True)
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    emit("user_left", {"username": session["username"]}, broadcast=True)
+    users.remove(session["username"])
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
