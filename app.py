dda12383f86d95bba444115f7ca3f931fb0ad8e3
amindridi1 <125443150+amindridi1@users.noreply.github.com>
2024-11-01 14:07:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app)
 
-# In-memory storage for messages
-messages = []
-
 @app.route('/')
 def index():
-    return render_template('index.html', messages=messages)
+    return render_template('index.html')
 
 @socketio.on('message')
 def handle_message(msg):
-    messages.append(msg)  # Store the message
-    send(msg, broadcast=True)  # Broadcast the message to all clients
+    print('Message: ' + msg)
+    send(msg, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app)
